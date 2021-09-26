8571a523ae827e9af43208d6b461e638dc5f62c9
Jérôme Odier <jerome.odier@lpsc.in2p3.fr>
2021-09-27 00:22:09 +0200

    update

--- src/wombat-iot.c
+++ src/wombat-iot.c
@@ -138,6 +138,7 @@ int main(int argc, char **argv)
 	STR_t descr = iot_config_get_str(&config, "iot::descr", IOT_DEFAULT_IOT_DESCR);
 	int_t sleep_ms = iot_config_get_int(&config, "iot::sleep_ms", IOT_DEFAULT_IOT_SLEEP_MS);
 	int_t log_level = iot_config_get_int(&config, "iot::log_level", IOT_DEFAULT_IOT_LOG_LEVEL);
+
 	STR_t mqtt_server_uri = iot_config_get_str(&config, "mqtt::server_url", IOT_DEFAULT_MQTT_SERVER_URL);
 	STR_t mqtt_server_user = iot_config_get_str(&config, "mqtt::server_user", IOT_DEFAULT_MQTT_SERVER_USER);
 	STR_t mqtt_server_pass = iot_config_get_str(&config, "mqtt::server_pass", IOT_DEFAULT_MQTT_SERVER_PASS);
